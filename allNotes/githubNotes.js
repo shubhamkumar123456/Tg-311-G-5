80f45a2b635fbdc13857a2aeb547ee09165c6276
shubham kumar <shubhamfarainzi@gmail.com>
2026-02-18 19:53:05 +0530

    added github code and updated folder for notes

---
+++ allNotes/githubNotes.js
@@ -0,0 +1,38 @@
+// Git -->git is a tool .it is a version control system taht help you save, track and manage changes in your code.
+
+// in simple word it is like a time machine for your project it remembers every changes you make, so you can go back to old version , fix mistakes and work with others safely
+
+
+// untracked area    ||    stagging area   ||   files To be commited
+
+
+// git commands -->
+// 1) create a folder -->mkdir folderName
+// 2) create a file --> touch fileName  or echo>fileName
+// 3) create a git repository --> git init
+// 4) add a file in stagging area --> git add fileName
+// 5) add all file in stagging area --> git add .
+// 6) save files permanently in .git folder (take a snapshot of your file and save its hostory) --> git commit -m "any message"
+// 7) show all commits --> git log
+// 8)git status -->shows which files are untracked , which files are in stagging area , files to be commited. it also show which branch you are on
+
+
+
+// Github --> it a website where you host you local git project
+// github commands -->
+// 1) create a account on github
+// 2) git config --global user.name "your github name"
+// 3) git config --global user.email "your github email"
+// 4) git config user.name --> it will show your github name if already added
+// 5) git config user.email --> it will show your github email if already added
+// 6) connect your local machine git project to online github project --> git remote add origin "online github repo link"
+
+// 7) rename current branch -->git branch -M newName
+// 8) push your local code to online github --> git push origin branchName
+// example --> git push origin main
+
+//9)remove connection from localrepo to online github repo --> git remote remove origin
+
+// 10)git branch --> show you all branches 
+// 11) git checkout breanchName --> switch to  other branch
+// 12)

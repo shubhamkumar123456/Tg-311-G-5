ea540981b702f6e2ca3389ce4fffdda39b0172ef
shubham kumar <shubhamfarainzi@gmail.com>
2026-04-29 18:19:21 +0530

    updated db.js

--- socialMedia/backend/config/db.js
+++ socialMedia/backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
 
 async function connection(){
    try {
-    await mongoose.connect('mongodb://localhost:27017/g5SocialMedia')
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@socialmediag5.pjeptuw.mongodb.net/?appName=socialMediaG5`)
+
    console.log("mongodb connected successfully")
    } catch (error) {
     console.log('error in connecting db', error)
